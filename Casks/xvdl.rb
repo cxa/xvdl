8cc0c27c83272cc94404d6eea77546295b501e8a
CHEN Xian-an <xianan.chen@gmail.com>
2026-09-07 14:22:32 +0800

    chore(homebrew): pin v260907.1 checksum

--- Casks/xvdl.rb
+++ Casks/xvdl.rb
@@ -1,6 +1,6 @@
 cask "xvdl" do
   version "260907.1"
-  sha256 :no_check
+  sha256 "178a6402574534ac1cd42b9a528b92106f80366747867d879f0a56131abc1e43"
 
   url "https://github.com/cxa/xvdl/releases/download/v#{version}/XVDL-#{version}-macos.zip"
   name "XVDL"
